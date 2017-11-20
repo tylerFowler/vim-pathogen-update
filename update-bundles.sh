be9e5e52f54c34e2dfc04b1eaeb66592ea4ef95c
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-11-19 19:47:55 -0600

    Accept optional arg for bundle dir

--- update-bundles.sh
+++ update-bundles.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+# Colors
 green='\033[0;032m'
 red='\033[0;031m'
 uncolor='\033[0m'
 
-if [ ! -d "${HOME}/.vim/bundle" ]; then
+default_bundle_dir=$HOME/.vim/bundle
+bundle_dir=${1:-$default_bundle_dir}
+
+echo "Using bundle dir ${bundle_dir}"
+
+if [ ! -d "${bundle_dir}" ]; then
   echo "${red}Could not find vim bundle directory at ${HOME}/.vim/bundle${uncolor}"
   exit 1
 fi
 
-for bundle in $HOME/.vim/bundle/*/; do
+for bundle in $bundle_dir/*/; do
   echo "${green}Updating `basename $bundle`${uncolor}"
   git -C "${bundle}" pull
 done
